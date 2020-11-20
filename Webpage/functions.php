ab1786767269a450d46298e3fad6d5714d20f578
Hayden Soong <soonghingfei@student.usm.my>
2020-11-20 18:31:54 +0800

    Updated version

--- Webpage/functions.php
+++ Webpage/functions.php
@@ -20,21 +20,25 @@ function list_file(){
     // loop through every available files
     foreach ($files as $file) {
         //echo "<p><a href='views.php?file=$file'> $file </a> </p>";
-        echo "<ul> <li><div class='list-of-files'><a class='file-button' href='views.php?file=$file'>$file </a> <a href='views.php?file=$file&action=1'><img class='trash-icon' src=image/trash.svg></a></div> </li></ul>";
+        echo "<ul> <li><div class='list-of-files'><a class='file-button' href='views.php?file=$file&action=0'>$file </a> <a href='views.php?file=$file&action=1'><img class='trash-icon' src=image/trash.svg></a></div> </li></ul>";
     }
-
 }
 
 // READ FILE
 function read_file(){
     // CHECK IF URL GOT PARAMETER
-    if (isset($_GET['file'])) {
-        {
-            // GET URL FILENAME
-           $file = $_GET['file'];
-           $open_file = fopen("storage/$file", "r");
-           echo fgets($open_file);
-           fclose ($open_file);
+    $action = $_GET['action'];
+    if ($action == 0) {
+        if (isset($_GET['file'])) {
+            {
+                // GET URL FILENAME
+                $file = $_GET['file'];
+                $open_file = fopen("storage/$file", "r");
+                while (!feof($open_file)) {
+                    echo fgets($open_file) . "<br />";
+                }
+                fclose ($open_file);
+            }
         }
     }
 }
@@ -42,7 +46,6 @@ function read_file(){
 function delete_file()
 {
     //GET FILE
-
     $action = $_GET['action'];
 
     if ($action == 1) {
@@ -56,7 +59,6 @@ function delete_file()
     else{
         return "";
     }
-
 }
 
 
